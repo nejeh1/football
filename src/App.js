09dde8102fe5915cdf35757fca971d2d23689373
nejeh1 <nejehchaabani11@gmail.com>
2023-09-11 10:23:42 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Playercard from './Playercard';
+import Players from './Players.js'
+
+function App() {
+    
+  return (
+    <div style={{display:"flex", flexWrap:"wrap", gap:"20px"}}>
+    {Players.map((player, index) => (
+        <Playercard key={index} Player={player} />
+      ))}
+    </div>
+  )  
+}
+
+export default App;
---
+++ src/Playercard.js
@@ -0,0 +1,25 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Players from './Players';
+
+function Playercard(props) {
+  
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+      <Card.Img variant="top" src={props.Player.image} alt={props.Player.name}/>
+        <Card.Title style={{fontSize:30}}>name:{props.Player.name}</Card.Title>
+        <div style={{display:'flex',gap:'40px'}}>
+        <Card.Text style={{fontSize:20}}>Number:{props.Player.Number}</Card.Text>
+        <Card.Text style={{fontSize:20}}>age:{props.Player.age}</Card.Text>
+        </div>
+        <Card.Text style={{fontSize:20}}>nationality:{props.Player.nationality}</Card.Text>
+        <Card.Text style={{fontSize:20}}>team:{props.Player.team}</Card.Text>
+        
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Playercard;
